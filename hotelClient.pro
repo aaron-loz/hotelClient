c2d8d6cf70b45d9f8e7bffc5acf8231ec530d1ed
dazdndcunfusd <dazdndcunfusd@gmail.com>
2016-06-16 12:12:30 -0400

    Started work on Client Program

---
+++ hotelClient.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-06-16T12:00:53
+#
+#-------------------------------------------------
+
+QT       += core gui
+QT       += network widgets
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = hotelClient
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+
+QT_BEGIN_NAMESPACE
+class QTcpSocket;
+class QNetworkSession;
+QT_END_NAMESPACE
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void requestHotelInfo;
+    void readHotelInfo;
+    //!Maybe have the connected slot in the server go to a different slot if needed
+    void requestClientInfo;
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket *tcpSocket;
+    QString clientData;
+    quint16 blocksize;
+    QNetworkSession *networkSession;
+};
+
+#endif // MAINWINDOW_H
